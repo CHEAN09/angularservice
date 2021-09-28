b5b96d28ee48cdd767fcd261ef777b16cea588ea
CHEAN09 <supermanverano@gmail.com>
2021-09-28 22:59:40 +0800

    first commit

--- src/app/participants/participants.component.ts
+++ src/app/participants/participants.component.ts
@@ -10,40 +10,21 @@ export class ParticipantsComponent  {
   display;
   participants;
 
-  /*constructor(){
-    let service = new ParticapantsService();
-    this.participants = service.getParticipants();
-  }*/
-  
-  /*showMale() {
-   for(let i = 0; i < this.participants.length; i++){
-      if(this.participants[i].gender ==='male'){
-        this.participants[i]);
-      }
-   }
-  }*/
   constructor(service:  ParticapantsService){
     this.participants = service.getParticipants();
     this.display = service.getParticipants();
   }
-  male(){
-    this.display = this.participants
+  showMale(){
+    this.display = this.participants;
     this.display = this.participants.filter(participant => participant.gender === 'Male');
 
   }
-  female(){
-    this.display = this.participants
+  showFemale(){
+    this.display = this.participants;
     this.display = this.participants.filter(participant => participant.gender === 'Male');
   }
   showAll(){
     this.display = this.participants;
   }
-  /*ShowFemale(){
-    for(let i = 0; i < this.participants.length; i++){
-      if(this.participants[i].gender ==='female'){
-        console.log(this.participants[i]);
-      }
-   }
-  }*/
  
 }
