a21f5417c9a56d5ccf1b25fd1cf5e3a4e83de676
CHEAN09 <supermanverano@gmail.com>
2021-09-28 22:46:06 +0800

    first commit

--- src/app/participants/participants.component.ts
+++ src/app/participants/participants.component.ts
@@ -1,5 +1,4 @@
 import { ParticapantsService } from './../participants.service';
-
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,21 +7,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./participants.component.css']
 })
 export class ParticipantsComponent  {
-  
+  display;
   participants;
 
-  constructor(){
+  /*constructor(){
     let service = new ParticapantsService();
     this.participants = service.getParticipants();
-  }
+  }*/
   
-  showMale() {
-   
+  /*showMale() {
+   for(let i = 0; i < this.participants.length; i++){
+      if(this.participants[i].gender ==='male'){
+        this.participants[i]);
+      }
+   }
+  }*/
+  constructor(service:  ParticapantsService){
+    this.participants = service.getParticipants();
+    this.display = service.getParticipants();
   }
-  ShowFemale(){
+  male(){
+    this.display = this.participants
+    this.display = this.participants.filter(participant => participant.gender === 'Male');
 
   }
-  ShowAll(){
-
+  female(){
+    this.display = this.participants
+    this.display = this.participants.filter(participant => participant.gender === 'Male');
   }
+  showAll(){
+    this.display = this.participants;
+  }
+  /*ShowFemale(){
+    for(let i = 0; i < this.participants.length; i++){
+      if(this.participants[i].gender ==='female'){
+        console.log(this.participants[i]);
+      }
+   }
+  }*/
+ 
 }
